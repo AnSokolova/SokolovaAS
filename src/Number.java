4fe93c3def51890e40134f58f446873358e4ee11
AnSokolova <Ania13n@gmail.com>
2020-09-29 14:34:33 +0300

    Задание 8

---
+++ src/Number.java
@@ -0,0 +1,9 @@
+//Задача 8
+
+public class Number {
+    public static void main(String[] args) {
+        for (int i=1; i <= 50; i++) {
+            System.out.print (i + " ");
+        }
+    }
+}
