27854ddd119aa66fe01d8c5192828492e415c26b
AnSokolova <Ania13n@gmail.com>
2020-10-05 12:44:51 +0300

    Задание 11

---
+++ src/VString.java
@@ -0,0 +1,12 @@
+//Задание 11
+
+import java.util.Scanner;
+
+public class VString {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        String str = scan.nextLine();
+        System.out.println(str);
+    }
+}
