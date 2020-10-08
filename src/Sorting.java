85386470cdd7ada662972e734d8c8a70647aef53
AnSokolova <Ania13n@gmail.com>
2020-10-08 14:01:49 +0300

    Задание 15

---
+++ src/Sorting.java
@@ -0,0 +1,31 @@
+//Задание 15
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Sorting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите длину массива: ");
+        int size = input.nextInt();
+        int array[] = new int[size];
+        System.out.println("Введите элементы массива:");
+        for (int i = 0; i < size; i++) {
+            array[i] = input.nextInt();
+        }
+        bubbleSort(array);
+    }
+
+    public static void bubbleSort(int[] array) {
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+            }
+        }
+        System.out.println("Отсортированный массив: " + Arrays.toString(array));
+    }
+}
