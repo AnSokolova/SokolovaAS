b25c460695383d10b25539e564b7001eb54ed58b
AnSokolova <Ania13n@gmail.com>
2020-10-10 17:01:44 +0300

    Задание 16

---
+++ src/OutText.java
@@ -0,0 +1,16 @@
+//Задание 16
+
+import java.io.*;
+
+public class OutText {
+    public static void main(String[] args) {
+        String S;
+        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Аня\\Desktop\\outTest.txt"))) {
+            while ((S = br.readLine()) != null) {
+                System.out.println(S);
+            }
+        } catch (IOException exc) {
+            System.out.println("Ошибка ввода вывода: " + exc);
+        }
+    }
+}
