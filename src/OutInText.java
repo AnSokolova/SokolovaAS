a40292534806605e52bb893444dc41265c37a04b
AnSokolova <Ania13n@gmail.com>
2020-10-15 23:57:47 +0300

    Задание 18

--- src/OutInText.java
+++ src/OutInText.java
@@ -5,25 +5,27 @@ import java.io.*;
 public class OutInText {
     public static void main(String[] args) {
         String OutS;
-        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Аня\\Desktop\\Test.txt"))) {
+        String InS;
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("OutInText.txt"));
             while ((OutS = br.readLine()) != null) {
                 System.out.println(OutS);
             }
-        } catch (IOException exc) {
-            System.out.println("Ошибка ввода вывода: " + exc);
-        }
-        String InS;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        try (FileWriter fw = new FileWriter("C:\\Users\\Аня\\Desktop\\Test.txt")){
-            do {
+            LineNumberReader lineNumberReader = new LineNumberReader(br);
+            int Outlines = 0;
+            while (lineNumberReader.readLine() != null){
+                Outlines++;
+            }
+            System.out.println(Outlines);
+            lineNumberReader.close();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            FileWriter fw = new FileWriter("OutInText.txt");
+            for (int i=0; i <= Outlines; i++){
                 System.out.print(": ");
-                InS = br.readLine();
-                OutS = br.readLine();
-                if (InS.length() == OutS.length()) break;
+                InS = bufferedReader.readLine();
                 InS = InS + "\r\n";
                 fw.write(InS);
             }
-            while (InS.length() != OutS.length());
         }
         catch (IOException exc) {
             System.out.println("Ошибка ввода вывода: " + exc);
